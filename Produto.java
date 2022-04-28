ca18f0bcd6cbef2b7714bff2c58e6f1c2b2c19a5
fefemuahahah <101228811+fefemuahahah@users.noreply.github.com>
2022-04-28 16:33:06 -0300

    Add files via upload

---
+++ Produto.java
@@ -0,0 +1,17 @@
+public class Produto {
+    String produto;
+    Integer quantidadeNoEstoque;
+
+
+    public Produto(String produto, Integer quantidadeNoEstoque) {
+        this.produto = produto;
+        this.quantidadeNoEstoque = quantidadeNoEstoque;
+        }
+        public  boolean temEstoque(){
+        if (this.quantidadeNoEstoque >= 10){
+            return true;
+        }
+        return false;
+        }
+
+}

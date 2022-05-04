07bc82980199a21bb823625a09bb648cb133f0d5
fefemuahahah <101228811+fefemuahahah@users.noreply.github.com>
2022-05-04 19:45:53 -0300

    Delete Exercicio02.java

--- Exercicio02.java
+++
@@ -1,6 +0,0 @@
-public class Exercicio02 {
-    public static void main(String[] args) {
-        Produto produto = new Produto("Galaxy S20", 8);
-        System.out.println(produto.temEstoque());
-    }
-}
